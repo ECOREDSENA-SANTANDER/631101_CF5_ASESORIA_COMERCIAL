2b6e57ef7f4da3880d9048c648a818c0991372a3
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-10-23 10:00:25 -0500

    Entrega Actividad Didáctica

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -15,29 +15,90 @@ export default {
   components: { Actividad },
   data: () => ({
     cuestionario: {
-      tema: 'Nombre del componente formativo',
+      tema: 'Ventas y comunicación',
       titulo: 'Cuestionario',
-      introduccion: '<b> Objetivo:</b> Texto de Word',
+      introduccion:
+        '<b> Objetivo:</b> Apropiarse de los conocimientos asociados a la venta y las transacciones comerciales, así como adquirir los necesarios para una comunicación efectiva.',
       barajarPreguntas: false,
       preguntas: [
         {
           id: 1,
-          texto: 'Texto pregunta 1 con <strong>Negrita</strong> <em>Itálica</.',
-          imagen: require('@/assets/componentes/pregunta_1.svg'),
+          texto:
+            'Según el ciclo de la venta, ¿cuál es el primer paso clave en el proceso de prospección?',
+          imagen: require('@/assets/actividad/img1.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: 'Opción 1', esCorrecta: true },
-            { id: 'b', texto: 'Opción 2', esCorrecta: false },
-            { id: 'c', texto: 'Opción 3', esCorrecta: false },
-            { id: 'd', texto: 'Opción 4', esCorrecta: false },
+            {
+              id: 'a',
+              texto: 'Elaborar una lista de clientes.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Identificar los posibles clientes en perspectiva.',
+              esCorrecta: true,
+            },
+            { id: 'c', texto: 'Hacer una visita inicial.', esCorrecta: false },
+            { id: 'd', texto: 'Cerrar la venta.', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
         },
         {
           id: 2,
-          texto: 'Texto pregunta 2',
-          imagen: require('@/assets/componentes/pregunta_2.png'),
+          texto:
+            '¿Qué tipo de ventas se caracteriza por usar una red de distribuidores que pueden reclutar a otros para vender productos?',
+          imagen: require('@/assets/actividad/img2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            { id: 'a', texto: 'Venta personal.', esCorrecta: false },
+            { id: 'b', texto: 'Venta multinivel.', esCorrecta: true },
+            { id: 'c', texto: 'Comercio electrónico.', esCorrecta: false },
+            {
+              id: 'd',
+              texto: 'Venta directa al consumidor.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 3,
+          texto:
+            '¿Cuál es la principal ventaja del comercio electrónico para las empresas?',
+          imagen: require('@/assets/actividad/img3.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Proporciona una red de distribuidores.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Facilita transacciones globales rápidas y seguras.',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'Depende únicamente de la interacción personal.',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Requiere siempre una tienda física.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 4,
+          texto:
+            'Una de las etapas importantes de la venta es la presentación, donde se deben resaltar los beneficios para el cliente.',
+          imagen: require('@/assets/actividad/img4.png'),
           barajarRespuestas: true,
           opciones: [
             { id: 'a', texto: 'Falso', esCorrecta: true },
@@ -47,15 +108,143 @@ export default {
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
         },
         {
-          id: 3,
-          texto: 'Texto pregunta 3',
-          imagen: require('@/assets/componentes/pregunta_3.png'),
+          id: 5,
+          texto:
+            'Una de las etapas importantes de la venta es la presentación, donde se deben resaltar los beneficios para el cliente.',
+          imagen: require('@/assets/actividad/img1.png'),
           barajarRespuestas: false,
           opciones: [
-            { id: 'a', texto: 'Opción 1', esCorrecta: true },
-            { id: 'b', texto: 'Opción 2', esCorrecta: false },
-            { id: 'c', texto: 'Opción 3', esCorrecta: false },
-            { id: 'd', texto: 'Todas las anteriores', esCorrecta: false },
+            { id: 'a', texto: 'Verdadero', esCorrecta: true },
+            { id: 'b', texto: 'Falso', esCorrecta: false },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 6,
+          texto:
+            '¿Qué papel juega la comunicación efectiva en el proceso de venta?',
+          imagen: require('@/assets/actividad/img2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto:
+                'Garantiza que los vendedores cumplan con sus metas personales.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Facilita la comprensión de las necesidades del cliente.',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto:
+                'Mejora únicamente la relación interna entre los vendedores.',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto:
+                'Aumenta la cantidad de productos vendidos sin esfuerzo adicional.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 7,
+          texto:
+            '¿Qué función cumple la productividad en ventas para los vendedores?',
+          imagen: require('@/assets/actividad/img3.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto:
+                'Mejorar únicamente los resultados financieros de la empresa.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto:
+                'Garantizar un control eficiente del tiempo y los recursos.',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto:
+                'Generar una relación más cercana entre el vendedor y el cliente.',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto:
+                'Aumentar la cantidad de productos ofrecidos sin planificación.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 8,
+          texto:
+            '¿Cómo afecta la claridad en las comunicaciones internas de una empresa al proceso de ventas?',
+          imagen: require('@/assets/actividad/img4.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto:
+                'Genera un aumento inmediato en las ventas sin importar otros factores.',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto:
+                'Facilita la coordinación y alineación entre el equipo de ventas.',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto:
+                'Reduce la importancia del entrenamiento en técnicas de venta.',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Hace que los clientes dependan más de la publicidad.',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 9,
+          texto:
+            'La transparencia en una transacción comercial depende exclusivamente de la relación del vendedor con el cliente.',
+          imagen: require('@/assets/actividad/img1.png'),
+          barajarRespuestas: false,
+          opciones: [
+            { id: 'a', texto: 'Verdadero', esCorrecta: false },
+            { id: 'b', texto: 'Falso', esCorrecta: true },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 10,
+          texto:
+            'Un vendedor efectivo utiliza la comunicación como una herramienta para comprender las necesidades del cliente y ofrecer soluciones adecuadas.',
+          imagen: require('@/assets/actividad/img2.png'),
+          barajarRespuestas: false,
+          opciones: [
+            { id: 'a', texto: 'Verdadero', esCorrecta: true },
+            { id: 'b', texto: 'Falso', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
