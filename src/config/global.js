6c027d041f0c87e36794e336a47e7fffa33bbc2b
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-12-04 09:26:44 -0500

    Cargue CFA

--- src/config/global.js
+++ src/config/global.js
@@ -170,7 +170,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF5_631101_DU.pdf',
       },
       {
         icono: 'fas fa-download',
